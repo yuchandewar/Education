3cf3aa3c5b0f10e8261413f166b3b61469d9a435
yuchandewar <103627557+yuchandewar@users.noreply.github.com>
2025-07-11 12:01:04 +0530

    Upload file ved.java for user 6870af3e0fd025fb7a250e3b

---
+++ 6870af3e0fd025fb7a250e3b/ai/ved.java
@@ -0,0 +1,15 @@
+class ved{
+   public static void main(String args[]){
+      int marks=92;
+     if(marks<85){
+     System.out.println("students get b grade");
+     }
+      else if(marks>90){
+      System.out.println("students get a grade");
+      }
+       else{
+       System.out.println("student get fail");
+       }
+     }
+    }
+      
